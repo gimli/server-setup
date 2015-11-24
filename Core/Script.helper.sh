fdaab41870566264f79e3852259707185a665652
Nickless <admin@isengard.dk>
2015-11-24 11:27:53 -0800

    a new way for the script helper..

--- Core/Script.helper.sh
+++ Core/Script.helper.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
-if [ $ARGS = "--help" ]; then
-  echo "availble commands.. --run-upgrade does as it says, upgrading script."
-  echo " * --help"
-  echo " * --run-upgrade"
-  echo " * --run-uninstall"
-  exit 0
-fi
-
-if [ $ARGS = "--run-upgrade" ]; then
-  echo "Updating server-setup script, please hold on.."
-  if [ -f /opt/server-setup ]; then
-    echo "updating dir: /opt/server-setup"
-    cd /opt/server-setup
-  else
-    echo "updating dir: ~/server-setup"
-    cd ~/server-setup
-  fi
-  git pull
-  exit 0
-fi
-
-if [ $ARGS = "--run-uninstall" ]; then
-   read -p "Are you sure you want to uninstall server-setup? (y/N) " uninstall
-   if [ $uninstall = "y" ]; then
+case $ARGS in
+  --help)
+       echo "Help. Here a list of the availble commands."
+       echo " * --help "
+       echo " * --run-upgrade "
+       echo " * --run-uninstall "
+       exit 0
+  ;;
+  --run-upgrade)
+       echo "Updating script..."
+       if [ -f /opt/server-setup ]; then
+         echo "upgrading /opt/server-setup"
+         cd /opt/server-setup
+       else
+         echo "upgrading ~/server-setup"
+         cd ~/server-setup
+      fi
+     exit 0
+  ;;
+  --run-uninstall)
+      echo "Uninstalling server-setup, please standby."
       apt-get purge server-setup
       rm -rf /opt/server-setup
       rm /usr/sbin/server-setup
-      cd;clear
-      echo "Script uninstalled! We Hope you enjoyed using the script."
       exit 0
-   fi
-   exit 0
-fi
+  ;;
+  *)
+      echo "Sorry i didnt understand your command, please try --help for more info."
+      exit 0
+  ;;
+esac
